cd29a337876e671675164eceabf9061157202088
Miguel Xochicale <perez.xochicale@gmail.com>
2021-03-26 12:32:34 +0000

    adds notes to gym/kalman-filter/code/python/demo/script.py

--- gym/kalman-filter/code/python/demo/script.py
+++ gym/kalman-filter/code/python/demo/script.py
@@ -1,5 +1,10 @@
+## Usage
 # conda activate Filters
 # python script.py
+
+## Reference
+# https://github.com/cyber-tuna/kalman_demo/blob/master/kalman_demo.py
+
 
 import cv2 as cv
 import numpy as np
@@ -8,12 +13,18 @@ cursor_x = 250
 cursor_y = 250
 start = False
 
-img_height = 1000
-img_width = 1000
+img_height = 700
+img_width = 700
 
 img = np.zeros((img_height, img_width, 3), np.uint8)
 
 kalman = cv.KalmanFilter(4, 2, 0, cv.CV_32F)
+#<KalmanFilter object>	=	cv.KalmanFilter(	dynamParams, measureParams[, controlParams[, type]]	)
+# Parameters
+#    dynamParams	Dimensionality of the state.
+#    measureParams	Dimensionality of the measurement.
+#    controlParams	Dimensionality of the control vector.
+#    type	Type of the created matrices that should be CV_32F or CV_64F. 
 
 def update_cursor_pos(event, x, y, flags, param):
 	global cursor_x, cursor_y, start, img
@@ -21,8 +32,13 @@ def update_cursor_pos(event, x, y, flags, param):
 		start = True
 		cursor_x = x
 		cursor_y = y
-		#corrected state (x(k)): x(k)=x'(k)+K(k)*(z(k)-H*x'(k)) 
 		kalman.statePost = 1. * np.array([[cursor_x],[cursor_y],[0.],[0.]])
+		#statePost means corrected state (x(k)): x(k)=x'(k)+K(k)*(z(k)-H*x'(k)) 
+		#Example:	
+		#[[577.]
+ 		#[320.]
+ 		#[  0.]
+ 		#[  0.]]
 	elif event == cv.EVENT_LBUTTONDBLCLK:
 		img = np.zeros((img_height, img_width, 3), np.uint8)
 	elif event == cv.EVENT_MOUSEMOVE:
@@ -35,21 +51,38 @@ if __name__ == "__main__":
 	cv.namedWindow("KalmanDemo")
 	cv.setMouseCallback('KalmanDemo', update_cursor_pos)
 
-	#state transition matrix (A) 
+	##state transition matrix (A) 
+	#[[1.  0.  0.7 0. ]
+	# [0.  1.  0.  0.7]
+	# [0.  0.  1.  0. ]
+	# [0.  0.  0.  1. ]]
 	kalman.transitionMatrix = np.array([[1.,0.,.7,0.],[0.,1.,0.,.7],[0.,0.,1.,0.],[0.,0.,0.,1.]])
-	
-	#measurement matrix (H) 
+
+	##measurement matrix (H) 
+	#[[1. 0. 0. 0.]
+ 	#[0. 1. 0. 0.]]	
 	kalman.measurementMatrix = np.array([[1.,0.,0.,0.],[0.,1.,0.,0.]])
 	
 	#process noise covariance matrix (Q) 
+	#[[0.0001 0.     0.     0.    ]
+	# [0.     0.0001 0.     0.    ]
+	# [0.     0.     0.0001 0.    ]
+	# [0.     0.     0.     0.0001]]
 	kalman.processNoiseCov = 1e-4 * np.eye(4)
-	
+
 	#measurement noise covariance matrix (R) 
-	kalman.measurementNoiseCov = 15 * np.eye(2)
+	#[[15.  0.]
+	# [ 0. 15.]]
+	kalman.measurementNoiseCov = 15 * np.eye(2)	
 	
 	#posteriori error estimate covariance matrix (P(k)): P(k)=(I-K(k)*H)*P'(k) 
+	#[[1. 0. 0. 0.]
+	# [0. 1. 0. 0.]
+        # [0. 0. 1. 0.]
+	# [0. 0. 0. 1.]]
 	kalman.errorCovPost = 1. * np.eye(4)
 
+	
 	while(True):
 		if(start):
 			measurement = 1. * np.array([[0.],[0.]])
@@ -63,8 +96,11 @@ if __name__ == "__main__":
 			#print(measurement)
 
 			prediction = kalman.predict() # Computes a predicted state. 
+			#print (prediction)
 			kalman.correct(measurement) # Updates the predicted state from the measurement. 
-			cv.circle(img, (int(measurement[0]) , int(measurement[1])),2, (255, 255, 0), thickness=-1) 
+			#print (kalman.correct(measurement))
+			
+			cv.circle(img, (int(measurement[0]) , int(measurement[1])),2, (255, 255, 0), thickness=4) 
 			cv.circle(img, (int(kalman.statePost[0]) , int(kalman.statePost[1])),2, (0, 255, 255), thickness=-1) 
 
 		cv.imshow("KalmanDemo", img)
