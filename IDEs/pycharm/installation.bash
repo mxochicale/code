2d15ef8a39a5f62aa1774ca79161c9fe77147fcd
Miguel Xochicale <perez.xochicale@gmail.com>
2021-06-01 21:49:53 +0100

    updates IDEs/pycharm/installation.bash

--- IDEs/pycharm/installation.bash
+++ IDEs/pycharm/installation.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#VERSION_LINK=https://download.jetbrains.com/python/pycharm-community-2020.3.1.tar.gz
-VERSION_LINK=https://download.jetbrains.com/python/pycharm-community-2021.1.tar.gz ## updated on Thu  8 Apr 00:41:56 BST 2021
+#https://download.jetbrains.com/python/pycharm-community-2020.3.1.tar.gz
+#https://download.jetbrains.com/python/pycharm-community-2021.1.tar.gz ## updated on Thu  8 Apr 00:41:56 BST 2021
+VERSION_LINK=https://download.jetbrains.com/python/pycharm-community-2021.1.1.tar.gz #Tue  1 Jun 21:43:57 BST 2021
 
 cd ~/Downloads
 wget $VERSION_LINK
