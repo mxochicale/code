6c84150b0118548a3e11c4af5832d4e8f4d55c4b
mxochicale <perez.xochicale@gmail.com>
2024-03-31 05:59:06 +0100

    adds video-to-ascii-cpp #65

---
+++ opencv/examples/cpp/video-to-ascii-cpp/src/main.cpp
@@ -0,0 +1,72 @@
+#include <opencv2/opencv.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+using namespace std;
+using namespace cv;
+
+#define VID_WIDTH 640
+#define VID_HEIGHT 480
+#define VIDEO_IN "/dev/video0"
+
+
+string pixelToASCII(int pixel_intensity) {
+  //const string ASCII_CHARS = "@$%#&!*+=-_.                ";
+  //const string ASCII_CHARS = "@#&!*+=-_.     ";
+  const string ASCII_CHARS = "   ._-=+*!&#%$@";
+  string s =
+      string(1, ASCII_CHARS[pixel_intensity * ASCII_CHARS.length() / 256]);
+  return s;
+}
+
+int main() {
+  VideoCapture cap(VIDEO_IN);
+
+  double fps = cap.get(CAP_PROP_FPS);
+
+  cout << fps << endl;
+
+  //int frame_duration_ms = 10000 / fps;
+  //int frame_duration_ms = 1000 / fps;  //DEFAULT
+  int frame_duration_ms = 100 / fps;
+  //int frame_duration_ms = 10 / fps;
+  //int frame_duration_ms = 1 / fps;
+
+  int width = 250;
+  int height = 50;
+
+  int frame_width = cap.get(CAP_PROP_FRAME_WIDTH);
+  int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
+  cout << frame_width << " " << frame_height << endl;
+  // 4096 2160
+  // 751 × 944
+
+  height = (width * frame_height / frame_width) * 0.4194;
+
+  Mat frame, gray_frame, resized_frame;
+
+  while (true) {
+    cap >> frame;
+    if (frame.empty())
+      break;
+
+    cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
+
+    resize(gray_frame, resized_frame, Size(width, height), 0, 0, INTER_LINEAR);
+
+    string ascii_frame;
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        ascii_frame += pixelToASCII(resized_frame.at<uchar>(i, j));
+      }
+      ascii_frame += "\n";
+    }
+
+    system("clear"); // to clear the console
+    cout << ascii_frame;
+    std::this_thread::sleep_for(std::chrono::milliseconds(frame_duration_ms));
+  }
+
+  return 0;
+}
