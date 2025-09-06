ae5e3f89bce818fc09733a966105a9ff625072f4
Miguel Xochicale, PhD <perez.xochicale@gmail.com>
2025-09-06 14:45:26 +0100

    85 - adds installation of uv #85 and examples in pytorch with cuda nvidia (#86)
    * adds installation of uv #85 and examples in pytorch with cuda nvidia
    * fix copilot reviews #85

--- pytorch/examples/hello-world-NN/hwnn.py
+++ pytorch/examples/hello-world-NN/hwnn.py
@@ -1,12 +1,15 @@
-## Reference
-# https://www.digitalocean.com/community/tutorials/introduction-to-pytorch-build-a-neural-network-to-recognize-handwritten-digits
+"""Reference: https://www.digitalocean.com/community/tutorials/introduction-to-pytorch-build-a-neural-network-to-recognize-handwritten-digits"""
 
-## Usage
-# conda activate aiVE
-# python hwnn.py
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+if device.type == 'cuda':
+    print("Is cuda available: ", torch.cuda.is_available())
+    print("Number of devices: ", torch.cuda.device_count())
+    print("Name of the device(0): ", torch.cuda.get_device_name(0))
 
 net = nn.Linear(1, 1)  # 1. Build a computation graph (a line!)
 optimizer = optim.SGD(net.parameters(), lr=0.1)  # 2. Setup optimizers
@@ -17,8 +20,7 @@ x, target = torch.randn((1,)), torch.tensor([0.])  # 4. Setup data
 for i in range(10):
     output = net(x)
     loss = criterion(output, target)
-    print(round(loss.item(), 2))
-
+    print("  loss", round(loss.item(), 3))
     net.zero_grad()
     loss.backward()
     optimizer.step()
